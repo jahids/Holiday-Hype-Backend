c927890db94b2702ae9b8f3fc781737d754b2961
jahids <evanjahid321@gmail.com>
2021-10-31 20:49:31 +0600

    new

--- index.js
+++ index.js
@@ -2,14 +2,16 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
+
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
 
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 // middleware
+// middleware wdkhfas
 
 app.use(cors());
 app.use(express.json());
